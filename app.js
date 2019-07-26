260271213122bd4c10a0ecf1ce50dd437fe5611e
kohanaa <selfishever@gmail.com>
2019-07-26 17:46:14 +0300

    basic pages

--- app.js
+++ app.js
@@ -9,5 +9,10 @@ app.set('view engine', 'handlebars');
 app.get('/', function (req, res) {
     res.render('home');
 });
-
+app.get('/site', function (req, res) {
+    res.render('site');
+});
+app.get('/game', function (req, res) {
+    res.render('game');
+});
 app.listen(3000);
